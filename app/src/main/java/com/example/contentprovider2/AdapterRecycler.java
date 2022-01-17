2b27930ab77495955339244d39211f1306f7300f
LalremLian <amongbt@gmail.com>
2022-01-17 15:37:23 +0600

    beta 0.01

---
+++ app/src/main/java/com/example/contentprovider2/AdapterRecycler.java
@@ -0,0 +1,54 @@
+package com.example.contentprovider2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdapterRecycler extends RecyclerView.Adapter<AdapterRecycler.ViewHolder> {
+
+    Context context;
+    List<ModelClass> modelClassList;
+
+    public AdapterRecycler(Context context, List<ModelClass> modelClassList) {
+        this.context = context;
+        this.modelClassList = modelClassList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_contact,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        ModelClass modelClass = modelClassList.get(position);
+        holder.name.setText(modelClass.getName());
+        holder.number.setText(modelClass.getNumber());
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelClassList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name,number;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.tvName);
+            number = itemView.findViewById(R.id.tvNumber);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/contentprovider2/ContentProviderActivity.java
@@ -0,0 +1,73 @@
+package com.example.contentprovider2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ContentProviderActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    AdapterRecycler adapter;
+    ArrayList<ModelClass> modelClassArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_provider);
+
+        recyclerView = findViewById(R.id.recyclerView1);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        getFetch();
+    }
+
+    private void getFetch() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},0);
+        }
+        ContentResolver resolver = getContentResolver(); //creating an resolver object.............
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
+        Cursor cursor = resolver.query(uri, null, null, null, null);
+
+        modelClassArrayList = new ArrayList<>();
+        modelClassArrayList.clear();
+        int counter = 0;
+
+        if (cursor.getCount()>0)
+        {
+            while (cursor.moveToNext())
+            {
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                @SuppressLint("Range") String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                ModelClass modelClass = new ModelClass(name, number);
+                modelClassArrayList.add(counter, modelClass);
+
+                counter++;
+            }
+        }
+        //Log.e("ContactArrayList", modelClassArrayList.toString());
+
+        adapter = new AdapterRecycler(ContentProviderActivity.this, modelClassArrayList);
+        recyclerView.setAdapter(adapter);
+    }
+}
